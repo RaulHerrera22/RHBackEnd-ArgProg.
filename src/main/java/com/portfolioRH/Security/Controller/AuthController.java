82e79e154fc1553bec0ed3b99be8ce3a15c0d70a
Raul Herrera <shaulihot@gmail.com>
2023-04-20 09:15:13 -0300

    changes 2

--- src/main/java/com/portfolioRH/Security/Controller/AuthController.java
+++ src/main/java/com/portfolioRH/Security/Controller/AuthController.java
@@ -34,7 +34,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
-@CrossOrigin (origins = "rh-frontend.web.app")
+@CrossOrigin (origins = "http://rh-frontend.web.app")
 
 public class AuthController {
     @Autowired
